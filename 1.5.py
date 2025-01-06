969f4d4dd5090cbb0dadb5c5b4c1ff25ce4e0705
mengale1 <alex.meng@mail.utoronto.ca>
2025-01-05 21:00:52 -0500

    Add Day 5, 6 Solutions and Add Day 7 Starter

---
+++ 1.5.py
@@ -0,0 +1,33 @@
+##############################################################################
+# Author: AlexanderTheMango
+# Date: January 5th, 2025
+# Descrpition: Shifting Letters II
+# Leetcode Problem Link: https://leetcode.com/problems/shifting-letters-ii/description/?envType=daily-question&envId=2025-01-05
+##############################################################################
+
+from typing import List
+
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        diff = [0] * (n + 1)
+
+        # Apply shifts using the difference array
+        for start, end, direction in shifts:
+            shift = 1 if direction == 1 else -1
+            diff[start] += shift
+            if end + 1 < n:
+                diff[end + 1] -= shift
+
+        # Apply shifts directly while computing prefix sums
+        result = []
+        current_shift = 0
+        for i in range(n):
+            current_shift += diff[i]
+            # Apply the net shift to the current character
+            new_char = chr(
+                (ord(s[i]) - ord('a') + current_shift) % 26 + ord('a'))
+            result.append(new_char)
+
+        return ''.join(result)
---
+++ 1.6.py
@@ -0,0 +1,30 @@
+##############################################################################
+# Author: AlexanderTheMango
+# Date: January 6th, 2025
+# Descrpition: Minimum Number of Operations to Move All Balls to Each Box
+# Leetcode Problem Link: https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/?envType=daily-question&envId=2025-01-06
+##############################################################################
+
+from typing import List
+
+
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        answer = [0] * n
+
+        # Left-to-right pass
+        balls = operations = 0
+        for i in range(n):
+            answer[i] = operations
+            balls += boxes[i] == '1'
+            operations += balls
+
+        # Right-to-left pass
+        balls = operations = 0
+        for i in range(n - 1, -1, -1):
+            answer[i] += operations
+            balls += boxes[i] == '1'
+            operations += balls
+
+        return answer

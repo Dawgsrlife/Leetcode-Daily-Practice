2a68a7ba574a001f657be198b259d987a03d95fa
mengale1 <alex.meng@mail.utoronto.ca>
2025-01-10 12:00:06 -0500

    Complete Day 10 (day 9 was is also complete)

--- 1.10.py
+++ 1.10.py
@@ -1,12 +1,43 @@
 ##############################################################################
 # Author: AlexanderTheMango
-# Date: January 7th, 2025
-# Descrpition: [add here]
-# Leetcode Problem Link: [add here]
+# Date: January 10th, 2025
+# Description: Word Subsets
+# Leetcode Problem Link: https://leetcode.com/problems/word-subsets/description/
 ##############################################################################
 
 from typing import List
 
 
 class Solution:
-    pass
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        def count_frequency(word: str) -> dict:
+            freq = {}
+
+            for char in word:
+                if char in freq:
+                    freq[char] += 1
+                else:
+                    freq[char] = 1
+            return freq
+
+        required = {}
+        for b in words2:
+            b_count = count_frequency(b)
+            for char, count in b_count.items():
+                if char in required:
+                    required[char] = max(required[char], count)
+                else:
+                    required[char] = count
+
+        universal = []
+        for a in words1:
+            a_count = count_frequency(a)
+            is_universal = True
+            for char, count in required.items():
+                if a_count.get(char, 0) < count:
+                    is_universal = False
+                    break
+            if is_universal:
+                universal.append(a)
+
+        return universal
